f1a729c530af4ca75df5cbc87ad30c9327f944cf
kumarjadhav998 <111350840+kumarjadhav998@users.noreply.github.com>
2022-08-18 02:05:20 +0530

    Added files
    28July folder added

---
+++ 28Julydemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _28Julydemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            division d1 = new division();
+
+            Console.WriteLine("Enter the year:");
+            int year = int.Parse(Console.ReadLine());
+            
+            if (year % 4 == 0)
+            Console.WriteLine(year + "is a leap year");
+            else
+            Console.WriteLine(year + " is not a leap year");
+            
+        }
+    }
+}
---
+++ 28Julydemo/division.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _28Julydemo
+{
+    class division
+    {
+        public division()
+        {
+            Console.WriteLine("Enter a number:");
+            int a = int.Parse(Console.ReadLine());
+            if (a % 5 == 0 & a % 11 == 0)
+                Console.WriteLine(a + "divisable by 5 and 11");
+            else
+                Console.WriteLine(a + "divisable not by 5 and 11");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
